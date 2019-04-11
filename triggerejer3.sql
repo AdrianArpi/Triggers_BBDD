91d26940eb1e2df003a4fc978ae92a1d77ae9b9c
Adrian Arpi <arpiadrian5@gmail.com>
2019-04-11 13:06:47 +0200

    Correccion de trigger

--- triggerejer3.sql
+++ triggerejer3.sql
@@ -1,5 +1,13 @@
-create or replace trigger nocambiesnombre before update of nombre on empleado for each row
+create or replace trigger nocambiesnombre before update of nombre,num_sup,salario on empleado for each row
 BEGIN
-		raise_application_error(-20374, 'No se permite modificar el nombre del empleado');
+	if updating('nombre') then
+		raise_application_error(-20474, 'No se permite modificar el nombre del empleado');
+	end if;
+	if updating('num_sup') then
+		raise_application_error(-20374, 'No se permite modificar el num_sup');
+	end if;
+	if :new.salario > :old.salario *1.1 then
+		raise_application_error(-20574, 'No se le puede subir tanto el salario');
+	end if;
 	end;
 /
